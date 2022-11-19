a55d92b874fb329e217728f745b38857776be6d8
Joseph Diza <josephm.diza@gmail.com>
2022-11-19 15:16:47 -0800

    Disable formatting

--- src/argp/argp.py
+++ src/argp/argp.py
@@ -23,136 +23,136 @@ ArgParser = typing.NewType("ArgParser", None)
 Command = typing.NewType("Command", ArgParser)
 Argp = typing.NewType("Argp", ArgParser)
 
-class OptionsFormatter():
-    ''' Formats command line options '''
-    def __init__(self, argp_args: list,
-                 format='{space:<{indent}}{short_flag:<{short_padding}}{long:<{long_padding}}{help}\n',
-                 indent=2, short_padding=6, long_padding=21, *args, **kwargs):
-        self.args = argp_args
-        self.format = format
-        self.indent = indent
-        self.format = format
-        self.short_padding = short_padding
-        self.long_padding = long_padding
-        self.msg = ''
+# class OptionsFormatter():
+    # ''' Formats command line options '''
+    # def __init__(self, argp_args: list,
+                 # format='{space:<{indent}}{short_flag:<{short_padding}}{long:<{long_padding}}{help}\n',
+                 # indent=2, short_padding=6, long_padding=21, *args, **kwargs):
+        # self.args = argp_args
+        # self.format = format
+        # self.indent = indent
+        # self.format = format
+        # self.short_padding = short_padding
+        # self.long_padding = long_padding
+        # self.msg = ''
 
-    def format_help(self):
-        for arg in self.args:
-            if isinstance(arg, Option):
-                # Default option format:
-                #   -v,  --version              Show program version
-                #   -sb, --sub                  Subscript text
-                self.msg += self.format.format(
-                    space=' ', indent=self.indent,
-                    short_padding=self.short_padding, long_padding=self.long_padding,
-                    short_flag=arg.short + ',', long=arg.long, help=arg.help)
-        return self.msg
+    # def format_help(self):
+        # for arg in self.args:
+            # if isinstance(arg, Option):
+                # # Default option format:
+                # #   -v,  --version              Show program version
+                # #   -sb, --sub                  Subscript text
+                # self.msg += self.format.format(
+                    # space=' ', indent=self.indent,
+                    # short_padding=self.short_padding, long_padding=self.long_padding,
+                    # short_flag=arg.short + ',', long=arg.long, help=arg.help)
+        # return self.msg
 
-class HelpFormatter():
-    DEFAULT_HEADER_FORMAT: str = inspect.cleandoc(
-        '''
-        {usage_indicator}: {usage}
-        {desc}\n
-        {arg_defs}
-        ''')
-    DEFAULT_INDICATOR = 'Usage'
+# class HelpFormatter():
+    # DEFAULT_HEADER_FORMAT: str = inspect.cleandoc(
+        # '''
+        # {usage_indicator}: {usage}
+        # {desc}\n
+        # {arg_defs}
+        # ''')
+    # DEFAULT_INDICATOR = 'Usage'
 
-    def __init__(self, arg_defs, prog='', usage='', desc='',
-                 options_formatter = None,
-                 usage_indicator=DEFAULT_INDICATOR, usage_format=DEFAULT_HEADER_FORMAT):
-                 # indent=2,
-                 # short_padding=6,
-                 # long_padding=21,
-        self.arg_defs = arg_defs
-        self.prog = prog
-        self.usage = usage
-        self.desc = desc
-        self.msg = ''
+    # def __init__(self, arg_defs, prog='', usage='', desc='',
+                 # options_formatter = None,
+                 # usage_indicator=DEFAULT_INDICATOR, usage_format=DEFAULT_HEADER_FORMAT):
+                 # # indent=2,
+                 # # short_padding=6,
+                 # # long_padding=21,
+        # self.arg_defs = arg_defs
+        # self.prog = prog
+        # self.usage = usage
+        # self.desc = desc
+        # self.msg = ''
 
-        # Format
-        self.options_formatter = OptionsFormatter(arg_defs) if options_formatter is None else options_formatter
-        self.usage_indicator = usage_indicator
-        self.usage_format = usage_format
+        # # Format
+        # self.options_formatter = OptionsFormatter(arg_defs) if options_formatter is None else options_formatter
+        # self.usage_indicator = usage_indicator
+        # self.usage_format = usage_format
 
-    def format_help(self):
-        prog = self.prog
-        usage_indicator = self.usage_indicator
-        usage = self.usage
-        desc = self.desc
+    # def format_help(self):
+        # prog = self.prog
+        # usage_indicator = self.usage_indicator
+        # usage = self.usage
+        # desc = self.desc
 
-        arg_defs = ''
+        # arg_defs = ''
 
-        # Copy usage_format, usage strings
-        usage_msg = self.usage[:]
-        desc_msg = self.desc[:]
-        msg = self.usage_format[:]
+        # # Copy usage_format, usage strings
+        # usage_msg = self.usage[:]
+        # desc_msg = self.desc[:]
+        # msg = self.usage_format[:]
 
-        options_msg: str = ''
-        cmds_msg: str = ''
+        # options_msg: str = ''
+        # cmds_msg: str = ''
 
-        arg_def: Command | Option
-        # TODO: Ideal command default format
-        # Command Format:
-        # command-to-option:
-        # id                    help
-        #   short, long         help
-        #   short, long         help
-        # command-to-command:
-        # id        help
-        #   short, long         help
-        #   id        help
-        #       short, long         help
+        # arg_def: Command | Option
+        # # TODO: Ideal command default format
+        # # Command Format:
+        # # command-to-option:
+        # # id                    help
+        # #   short, long         help
+        # #   short, long         help
+        # # command-to-command:
+        # # id        help
+        # #   short, long         help
+        # #   id        help
+        # #       short, long         help
 
-        for arg_def in self.arg_defs:
-            # TODO: This solution only parses one layer of options for commands
-            # TODO: Ideally this would recursively parse all command options
-            if isinstance(arg_def, Command):
-                space = ' '
-                id = arg_def.id
-                help = arg_def.help
+        # for arg_def in self.arg_defs:
+            # # TODO: This solution only parses one layer of options for commands
+            # # TODO: Ideally this would recursively parse all command options
+            # if isinstance(arg_def, Command):
+                # space = ' '
+                # id = arg_def.id
+                # help = arg_def.help
 
-                format = ''
-                format = f'{space:<2}{id:<27}{help}'
-                cmds_msg += format
+                # format = ''
+                # format = f'{space:<2}{id:<27}{help}'
+                # cmds_msg += format
 
-                cmd_options_msg = ''
-                # TODO: Refactor this
-                for cmd_arg_def in arg_def.args:
-                    if isinstance(cmd_arg_def, Option):
-                        space = ' '
-                        short = cmd_arg_def.short
-                        long = cmd_arg_def.long
-                        help = cmd_arg_def.help
+                # cmd_options_msg = ''
+                # # TODO: Refactor this
+                # for cmd_arg_def in arg_def.args:
+                    # if isinstance(cmd_arg_def, Option):
+                        # space = ' '
+                        # short = cmd_arg_def.short
+                        # long = cmd_arg_def.long
+                        # help = cmd_arg_def.help
 
-                        short_flag = short + ','
-                        # TODO: These should not be hardcoded
-                        format = f'{space:<4}{short_flag:<6}{long:<19}{help}\n'
-                        cmd_options_msg += format
-                cmds_msg +=  '\n' + cmd_options_msg + '\n'
+                        # short_flag = short + ','
+                        # # TODO: These should not be hardcoded
+                        # format = f'{space:<4}{short_flag:<6}{long:<19}{help}\n'
+                        # cmd_options_msg += format
+                # cmds_msg +=  '\n' + cmd_options_msg + '\n'
 
-        options_msg += self.options_formatter.format_help()
+        # options_msg += self.options_formatter.format_help()
 
-        logger.debug(f'{prog=} {desc=} {usage=}')
-        logger.debug(cmds_msg)
-        logger.debug(options_msg)
+        # logger.debug(f'{prog=} {desc=} {usage=}')
+        # logger.debug(cmds_msg)
+        # logger.debug(options_msg)
 
-        # Format the help message
-        usage_msg = usage_msg.format(prog=prog)
-        desc_msg = desc_msg.format(prog=prog.capitalize())
+        # # Format the help message
+        # usage_msg = usage_msg.format(prog=prog)
+        # desc_msg = desc_msg.format(prog=prog.capitalize())
 
-        arg_defs = 'Commands\n' + cmds_msg + 'Options\n' + options_msg if cmds_msg != '' else options_msg
-        msg = msg.format(usage_indicator=usage_indicator, usage=usage_msg, desc=desc_msg, arg_defs=arg_defs)
+        # arg_defs = 'Commands\n' + cmds_msg + 'Options\n' + options_msg if cmds_msg != '' else options_msg
+        # msg = msg.format(usage_indicator=usage_indicator, usage=usage_msg, desc=desc_msg, arg_defs=arg_defs)
 
-        msg = msg.rstrip() # Remove last newline
-        self.msg = msg
+        # msg = msg.rstrip() # Remove last newline
+        # self.msg = msg
 
-    def show_usage(self):
-        self.format_help()
-        print(self.msg)
-        sys.exit(1)
+    # def show_usage(self):
+        # self.format_help()
+        # print(self.msg)
+        # sys.exit(1)
 
 class Option():
-    def __init__(self, short: str, long: str, val: typing.Any = None, flag=False,
+    def __init__(self, short: str, long: str, id:str = '', val: typing.Any = None, flag = False,
                  cb: typing.Callable = None, help='', *args, **kwargs):
         self.argids = ArgID(self, [short, long, id])
         self.flag = flag
@@ -269,10 +269,10 @@ class Argp():
         self.argp = ArgsMap(cli_defs)
 
         # Create HelpFormatter if not already made
-        if help_formatter == None:
-            self.help = HelpFormatter(arg_defs=cli_defs, prog=os.path.basename(sys.argv[0]), usage=usage, desc=desc)
-            help_option = Option('-h', '--help', cb=self.help.show_usage, help='Show program usage')
-            cli_defs.append(help_option)
+        # if help_formatter == None:
+            # self.help = HelpFormatter(arg_defs=cli_defs, prog=os.path.basename(sys.argv[0]), usage=usage, desc=desc)
+            # help_option = Option('-h', '--help', cb=self.help.show_usage, help='Show program usage')
+            # cli_defs.append(help_option)
 
         self.raw_args = sys.argv[1:]
         self.desc = desc
